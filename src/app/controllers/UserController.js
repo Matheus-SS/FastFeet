ba80af1f1be06eb6bd9ddfdcf77ef816ed70904b
Matheus Santos <matheus_sa_santos@hotmail.com>
2020-02-13 13:13:56 -0300

    authentication JWT done

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,13 +8,17 @@ class UserController {
       return res.status(400).json({ error: 'User already exists.' });
     }
 
-    const { id, name, email, password_hash } = await User.create(req.body);
+    const { id, name, email } = await User.create(req.body);
     return res.json({
       id,
       name,
       email,
-      password_hash,
     });
+  }
+
+  async update(req, res) {
+    console.log(req.userLoggedId);
+    return res.json('ok');
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
+import authenticationMiddleware from './app/middlewares/authentication';
 const routes = new Router();
 
 routes.post('/user', UserController.store);
+routes.post('/session', SessionController.store);
+
+routes.use(authenticationMiddleware);
+
+routes.put('/user', UserController.update);
 
 export default routes;

3ee1e14c1fa3312e67f0fb5c2ec1531948c39c79
Matheus Santos <matheus_sa_santos@hotmail.com>
2020-02-16 14:50:12 -0300

    crud recipient done

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -2,19 +2,41 @@ import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
-  async store (req, res) {
-    const schema = Yup.object ().shape ({
-      name: Yup.string ().required (),
-      street: Yup.string ().required (),
-      number: Yup.number ().integer ().positive ().required (),
-      complement: Yup.string ().required (),
-      state: Yup.string ().required (),
-      city: Yup.string ().required (),
-      zip_code: Yup.string ().required (),
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number()
+        .integer()
+        .positive()
+        .required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zip_code: Yup.string().required(),
     });
 
-    if (!await schema.isValid (req.body)) {
-      return res.status (400).json ({error: 'Validation failed'});
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const recipientClone = req.body;
+
+    //checks if there is a recipient equal to the one that will be saved
+    const recipientExist = await Recipient.findOne({
+      where: {
+        name: recipientClone.name,
+        street: recipientClone.street,
+        number: recipientClone.number,
+        complement: recipientClone.complement,
+        state: recipientClone.state,
+        city: recipientClone.city,
+        zip_code: recipientClone.zip_code,
+      },
+    });
+
+    if (recipientExist) {
+      return res.status(400).json({ error: 'Recipient already exists' });
     }
 
     const {
@@ -25,9 +47,9 @@ class RecipientController {
       state,
       city,
       zip_code,
-    } = await Recipient.create (req.body);
+    } = await Recipient.create(req.body);
 
-    return res.json ({
+    return res.json({
       name,
       address: {
         street,
@@ -40,12 +62,47 @@ class RecipientController {
     });
   }
 
-  async update (req, res) {
-    const recipient = await Recipient.findByPk (req.params.id);
-    // testar se exist recipient igual usar isEqual
-    if (!recipient) {
-      return res.status (400).json ({error: 'Recipient does not exists'});
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number()
+        .integer()
+        .positive(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zip_code: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
     }
+
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not exists' });
+    }
+
+    const recipientClone = req.body;
+    //checks if there is a recipient equal to the one that will be updated
+    const recipientExist = await Recipient.findOne({
+      where: {
+        name: recipientClone.name,
+        street: recipientClone.street,
+        number: recipientClone.number,
+        complement: recipientClone.complement,
+        state: recipientClone.state,
+        city: recipientClone.city,
+        zip_code: recipientClone.zip_code,
+      },
+    });
+
+    if (recipientExist) {
+      return res.status(400).json({ error: 'Recipient already exists' });
+    }
+
     const {
       name,
       street,
@@ -54,9 +111,9 @@ class RecipientController {
       state,
       city,
       zip_code,
-    } = await recipient.update (req.body);
+    } = await recipient.update(req.body);
 
-    return res.json ({
+    return res.json({
       name,
       address: {
         street,
@@ -67,6 +124,33 @@ class RecipientController {
         zip_code,
       },
     });
+  }
+
+  async index(req, res) {
+    const recipient = await Recipient.findAll();
+
+    return res.json(recipient);
+  }
+
+  async show(req, res) {
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not exists' });
+    }
+    return res.json(recipient);
+  }
+
+  async delete(req, res) {
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not exists' });
+    }
+
+    await recipient.destroy();
+
+    return res.json(recipient);
   }
 }
-export default new RecipientController ();
+export default new RecipientController();
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -7,17 +7,20 @@ import RecipientController from './app/controllers/RecipientController';
 import authenticationMiddleware from './app/middlewares/authentication';
 import Recipient from './app/models/Recipient';
 
-const routes = new Router ();
+const routes = new Router();
 
-routes.post ('/user', UserController.store);
-routes.post ('/session', SessionController.store);
+routes.post('/user', UserController.store);
+routes.post('/session', SessionController.store);
 
-routes.use (authenticationMiddleware); // authentication middleware
+routes.use(authenticationMiddleware); // authentication middleware
 // every routes below need authentication
 
-routes.put ('/user', UserController.update);
+routes.put('/user', UserController.update);
 
-routes.post ('/recipient', RecipientController.store);
-routes.put ('/recipient/:id', RecipientController.update);
+routes.post('/recipient', RecipientController.store);
+routes.put('/recipient/:id', RecipientController.update);
+routes.get('/recipient', RecipientController.index);
+routes.get('/recipient/:id', RecipientController.show);
+routes.delete('/recipient/:id', RecipientController.delete);
 
 export default routes;

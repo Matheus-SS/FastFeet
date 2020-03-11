a6656cf69a3385c39bf365f23d0bb17214ed4c0d
Matheus <matheus_sa_santos@hotmail.com>
2020-03-11 14:55:07 -0300

    configuring sending email

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,7 +1,9 @@
 import Order from '../models/Order';
 import Deliveryman from '../models/Deliveryman';
-import File from '../models/File';
+import Recipient from '../models/Recipient';
 import * as Yup from 'yup';
+
+import Mail from '../../lib/Mail';
 
 class OrderController {
   async store(req, res) {
@@ -19,10 +21,35 @@ class OrderController {
       return res.status(400).json({ error: 'Validation failed' });
     }
 
-    //create Orders
-    const { product_name, recipient_id, deliveryman_id } = await Order.create(
-      req.body
-    );
+    const { recipient_id, deliveryman_id } = req.body;
+
+    const recipientExists = await Recipient.findOne({
+      where: { id: recipient_id },
+    });
+
+    //checks if does not exists a recipient with the id that is on req.body
+    if (!recipientExists) {
+      return res.status(400).json({ error: 'Recipient does not exists' });
+    }
+
+    const deliverymanExists = await Deliveryman.findOne({
+      where: { id: deliveryman_id },
+    });
+
+    //checks if does not exists a deliveryman with the id that is on req.body
+    if (!deliverymanExists) {
+      return res.status(400).json({ error: 'Deliveryman does not exists' });
+    }
+
+    // create Orders
+
+    const { product_name } = await Order.create(req.body);
+
+    await Mail.sendMail({
+      to: `${deliverymanExists.name} <${deliverymanExists.email}`,
+      subject: 'Produto disponível para retirada',
+      text: 'Produto pronto para entrega',
+    });
 
     return res.json({
       product_name,
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e437fe301508f4',
+    pass: 'ff1cff0bfe16d9',
+  },
+  default: {
+    from: 'Equipe FastFeet <noreply@fastfeet.com>',
+  },
+};
